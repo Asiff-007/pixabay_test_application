0429e9c4f9b5d24c4d18793dc933b93c2e2977fa
Asiff-007 <asiffma1995@gmail.com>
2022-07-04 12:43:58 +0530

    This commit after internet permission to release and some alignment changes in view

--- lib/view/HomePage.dart
+++ lib/view/HomePage.dart
@@ -23,98 +23,99 @@ class HomePage extends StatelessWidget {
         title: Text("Image Search"),
       ),
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: 30, bottom: 15),
-            height: 50,
-            width: 320,
-            child: TextField(
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(borderSide: BorderSide(width: 2)),
-                  labelText: 'Search Images'),
-              onChanged: (Value) {
-                searchKey = Value;
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+        Container(
+          margin: const EdgeInsets.only(top: 30, bottom: 15),
+          height: height*0.0677,
+          width: width*.889,
+          child: TextField(
+            decoration: const InputDecoration(
+                border: OutlineInputBorder(borderSide: BorderSide(width: 2)),
+                labelText: 'Search Images'),
+            onChanged: (Value) {
+              searchKey = Value;
+            },
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(width*.022),
+            color: const Color(0xff38b5ed),
+          ),
+          height: height * .06,
+          width: width * .325,
+          child: FlatButton(
+              onPressed: () {
+                controller.getData(searchKey);
               },
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              color: const Color(0xff38b5ed),
-            ),
-            height: height * .06,
-            width: width * .225,
-            child: FlatButton(
-                onPressed: () {
-                  controller.getData(searchKey);
-                },
-                child: Text(
-                  'Search',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'poppins',
-                      fontSize: width * .04),
-                )),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Obx(() {
-            return Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 40, right: 40),
-                child: controller.loading == true
-                    ? const SizedBox(
-                        height: 100,
-                        width: 100,
-                        child: Center(
-                          child: CircularProgressIndicator(),
-                        ),
-                      )
-                    : controller.dataList.length == 0
-                        ? const Center(
-                            child: Text(
-                            'No Imge to show',
-                            style: TextStyle(color: Colors.black, fontSize: 15),
-                          ))
-                        : ListView.separated(
-                            shrinkWrap: true,
-                            itemCount: controller.dataList.length,
-                            itemBuilder: (context, index) {
-                              return FullScreenWidget(
-                                child: SafeArea(
-                                  child: Container(
-                                    height: 200,
-                                    width: 100,
-                                    child: Image.network(
-                                      controller.dataList[index].largeUrl
-                                          .toString(),
-                                      height: 200,
-                                      fit: BoxFit.fitWidth,
-                                      loadingBuilder: (context,child,loadingProgress){
-                                        if (loadingProgress ==null){
-                                          return child;
-                                        }
-                                        else{
-                                          return Center(child: CircularProgressIndicator());
-                                        }
-                                      },
-                                    ),
+              child: Text(
+                'Search',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'poppins',
+                    fontSize: width * .04),
+              )),
+        ),
+        SizedBox(
+          height: height*.027,
+        ),
+        Obx(() {
+          return Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(left: width * .111, right: width *.111),
+              child: controller.loading == true
+                  ? SizedBox(
+                      height: height *.135,
+                      width: width*.277,
+                      child: const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    )
+                  : controller.dataList.length == 0
+                      ? Center(
+                          child: Text(
+                          'No Imge to show',
+                          style: TextStyle(color: Colors.black, fontSize: width*.0416),
+                        ))
+                      : ListView.separated(
+                          shrinkWrap: true,
+                          itemCount: controller.dataList.length,
+                          itemBuilder: (context, index) {
+                            return FullScreenWidget(
+                              child: SafeArea(
+                                child: Container(
+                                  height: height*.271,
+                                  width: width*.277,
+                                  child: Image.network(
+                                    controller.dataList[index].largeUrl
+                                        .toString(),
+                                    height: height*.271,
+                                    fit: BoxFit.fitWidth,
+                                    loadingBuilder: (context,child,loadingProgress){
+                                      if (loadingProgress ==null){
+                                        return child;
+                                      }
+                                      else{
+                                        return Center(child: CircularProgressIndicator());
+                                      }
+                                    },
                                   ),
                                 ),
-                              );
-                            },
-                            separatorBuilder: (context, index) =>
-                                const SizedBox(
-                                  height: 10,
-                                )),
-              ),
-            );
-          }),
-        ],
-      )),
+                              ),
+                            );
+                          },
+                          separatorBuilder: (context, index) =>
+                              SizedBox(
+                                height: height*.0135,
+                              )),
+            ),
+          );
+        }),
+          ],
+        ),
+      ),
     );
   }
 }
